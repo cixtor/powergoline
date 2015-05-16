0b0c9a31df1fab7300c99f97a20100571d6a3eaf
Cixtor <cixtords@gmail.com>
2015-05-16 09:00:30 -0400

    Fixed. Home directory arrow and next color

--- powergoline.go
+++ powergoline.go
@@ -25,12 +25,21 @@ func (pogol PowerGoLine) WorkingDirectory() {
 	var homedir string = os.Getenv("HOME")
 	var workingdir string = os.Getenv("PWD")
 	var shortdir string = strings.Replace(workingdir, homedir, "", -1)
-	var segments []string = strings.Split(shortdir, "/")
-	var ttl_segms int = len(segments)
-	var last_segm int = (ttl_segms - 1)
+	var cleandir string = strings.Trim(shortdir, "/")
 
+	// Print the user home directory path.
 	fmt.Printf("\033[38;5;255;48;5;161m ~ \033[0m")
-	fmt.Printf("\033[38;5;161;48;5;238m\ue0b0\033[0m")
+
+	if cleandir == "" {
+		fmt.Printf("\033[38;5;161;48;5;070m\ue0b0\033[0m")
+	} else {
+		fmt.Printf("\033[38;5;161;48;5;238m\ue0b0\033[0m")
+	}
+
+	// Print the sequence of folders of the current path.
+	var segments []string = strings.Split(cleandir, "/")
+	var ttlsegms int = len(segments)
+	var last_segm int = (ttlsegms - 1)
 
 	for key, folder := range segments {
 		if folder != "" {
