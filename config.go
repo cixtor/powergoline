c74a608a7e3c4464c9e09fa413946c93b9061121
Cixtor <cixtords@gmail.com>
2015-05-20 09:00:30 -0400

    Added. Default status code colors

---
+++ config.go
@@ -0,0 +1,18 @@
+package main
+
+const config_path = ".powergoline.json"
+const username_fg = "255"
+const username_bg = "006"
+const hostname_fg = "255"
+const hostname_bg = "012"
+const home_directory_fg = "255"
+const home_directory_bg = "161"
+const working_directory_fg = "251"
+const working_directory_bg = "238"
+const status_success = "070"
+const status_failure = "001"
+const status_misuse = "003"
+const status_permission = "004"
+const status_not_found = "014"
+const status_invalid_exit = "008"
+const status_terminated = "013"
