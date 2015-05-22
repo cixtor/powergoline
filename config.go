0a6db90a7f48e75eac8408662d85cb0425b42976
Cixtor <cixtords@gmail.com>
2015-05-22 09:00:30 -0400

    Added. Color struct with default information getter

--- config.go
+++ config.go
@@ -20,6 +20,28 @@ const status_terminated = "013"
 type Configuration struct {
 }
 
+type PowerColor struct {
+	UsernameFg         string      `json:"username_fg"`
+	UsernameBg         string      `json:"username_bg"`
+	HostnameFg         string      `json:"hostname_fg"`
+	HostnameBg         string      `json:"hostname_bg"`
+	HomeDirectoryFg    string      `json:"home_directory_fg"`
+	HomeDirectoryBg    string      `json:"home_directory_bg"`
+	WorkingDirectoryFg string      `json:"working_directory_fg"`
+	WorkingDirectoryBg string      `json:"working_directory_bg"`
+	Status             StatusColor `json:"status"`
+}
+
+type StatusColor struct {
+	Success     string `json:"success"`
+	Failure     string `json:"failure"`
+	Misuse      string `json:"misuse"`
+	Permission  string `json:"permission"`
+	NotFound    string `json:"not_found"`
+	InvalidExit string `json:"invalid_exit"`
+	Terminated  string `json:"terminated"`
+}
+
 func (config Configuration) Path() string {
 	var homedir string = os.Getenv("HOME")
 
@@ -39,4 +61,26 @@ func (config Configuration) Exists() (bool, error) {
 	}
 
 	return false, err
+}
+
+func (config Configuration) Default() PowerColor {
+	var pcolor PowerColor
+
+	pcolor.UsernameFg = username_fg
+	pcolor.UsernameBg = username_bg
+	pcolor.HostnameFg = hostname_fg
+	pcolor.HostnameBg = hostname_bg
+	pcolor.HomeDirectoryFg = home_directory_fg
+	pcolor.HomeDirectoryBg = home_directory_bg
+	pcolor.WorkingDirectoryFg = working_directory_fg
+	pcolor.WorkingDirectoryBg = working_directory_bg
+	pcolor.Status.Success = status_success
+	pcolor.Status.Failure = status_failure
+	pcolor.Status.Misuse = status_misuse
+	pcolor.Status.Permission = status_permission
+	pcolor.Status.NotFound = status_not_found
+	pcolor.Status.InvalidExit = status_invalid_exit
+	pcolor.Status.Terminated = status_terminated
+
+	return pcolor
 }
