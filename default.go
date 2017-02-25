ef5e1222e0146bf0e4b186a9cfb7160bd10bbc0f
Cixtor <cixtords@gmail.com>
2017-02-25 09:00:00 -0600

    Modify colors for the default theme to a gray scale

--- default.go
+++ default.go
@@ -3,37 +3,40 @@ package main
 // External configuration file path.
 const configPath string = ".powergoline.json"
 
+// Define default datetime values.
+const datetimeST string = "enabled"
+const datetimeFG string = "255"
+const datetimeBG string = "232"
+
 // Define default username values.
 const usernameST string = "enabled"
 const usernameFG string = "255"
-const usernameBG string = "006"
+const usernameBG string = "235"
 
 // Define default hostname values.
 const hostnameST string = "enabled"
 const hostnameFG string = "255"
-const hostnameBG string = "012"
+const hostnameBG string = "238"
 
 // Define default working directory values.
 const maximumSegments string = "2"
 const homeDirFG string = "255"
-const homeDirBG string = "161"
-const workingDirFG string = "251"
-const workingDirBG string = "238"
+const homeDirBG string = "241"
+const workingDirFG string = "255"
+const workingDirBG string = "244"
 const rdonlyDirFG string = "255"
 const rdonlyDirBG string = "124"
 
-// Define default datetime values.
-const datetimeST string = "disabled"
-const datetimeFG string = "255"
-const datetimeBG string = "023"
-
-// Define default status symbols.
-const symbolRegular string = "$"
-const symbolSuperUser string = "#"
+// CVS default colors.
+const repositoryST string = "enabled"
+const repositoryFG string = "255"
+const repositoryBG string = "247"
+const repositoryCommittedBG string = "214"
+const repositoryUntrackedBG string = "175"
 
 // Status code default colors.
 const statusSymbol string = "255"
-const statusSuccess string = "070"
+const statusSuccess string = "247"
 const statusFailure string = "001"
 const statusMisuse string = "003"
 const statusPermission string = "004"
@@ -41,9 +44,6 @@ const statusNotFound string = "014"
 const statusInvalidExit string = "202"
 const statusTerminated string = "013"
 
-// CVS default colors.
-const repositoryST string = "enabled"
-const repositoryFG string = "000"
-const repositoryBG string = "148"
-const repositoryCommittedBG string = "214"
-const repositoryUntrackedBG string = "175"
+// Define default status symbols.
+const symbolRegular string = "$"
+const symbolSuperUser string = "#"
